9fbb162cb028f0a2b039c887d91a72fd9170c57e
Etienne Bontemps <etienne.bontemps@grenoble-inp.org>
2017-12-18 16:59:58 +0100

    beginning of letrecs unnesting

--- compiler/fclosure.ml
+++ compiler/fclosure.ml
@@ -23,14 +23,14 @@ type t =
     | IfBool of t * t * t
     | Let of (Fid.t * Ftype.t) * t * t
     | Var of Fid.t
-    | LetRec of Fsyntax.fundef * t
+    | LetRec of fundef * t
     | Tuple of t list
     | LetTuple of (Fid.t * Ftype.t) list * t * t
     | Array of t * t
     | Get of t * t
     | Put of t * t * t
 and fundef = {
-                name : Fid.l * Ftype.t;
+                name : Fid.t * Ftype.t;
                 args : (Fid.t * Ftype.t) list;
                 formal_fv : (Fid.t * Ftype.t) list;
                 body : t
@@ -40,45 +40,55 @@ and fundef = {
 
 (* type toplevel = fundef list *)
 
+let rec clos_exp (k:Fknormal.t) :t = match k with
+    | Unit -> Unit
+    | Bool a -> Bool a
+    | Int a ->  Int  a
+    | Float a -> Float a
+    | Not b -> Not (clos_exp b)
+    | Neg b -> Neg (clos_exp b)
+    | Sub (a, b) -> Sub (clos_exp a, clos_exp b)
+    | Add (a, b) -> Add (clos_exp a, clos_exp b)
+    | FAdd (a, b) -> FAdd (clos_exp a, clos_exp b)
+    | FNeg b -> FNeg (clos_exp b)
+    | FSub (a, b) -> FSub (clos_exp a, clos_exp b)
+    | FMul (a, b) -> FMul (clos_exp a, clos_exp b)
+    | FDiv (a, b) -> FDiv (clos_exp a, clos_exp b)
+    | Eq (a, b) -> Eq (clos_exp a, clos_exp b)
+    | LE (a, b) -> LE (clos_exp a, clos_exp b)
+    | Var a -> Var a
+    (* |App (a,b) -> AppD (clos_exp a, List.map clos_exp b) *)
+    | IfEq (a, b, c) -> IfEq (clos_exp a, clos_exp b, clos_exp c)
+    | IfLE (a, b, c) -> IfLE (clos_exp a, clos_exp b, clos_exp c)
+    (* |IfBool (a, b, c) -> IfBool (clos_exp a, clos_exp b, clos_exp c) *)
+    | Tuple a -> Tuple (List.map clos_exp a)
+    (* |LetTuple (a, b, c) -> LetTuple (clos_exp a, clos_exp b, clos_exp c) *)
+    | Array (a, b) -> Array (clos_exp a, clos_exp b)
+    | Get (a, b) -> Get (clos_exp a, clos_exp b)
+    | Put (a, b, c) -> Put (clos_exp a, clos_exp b, clos_exp c)
+    (*/tmp*)
 
 (* Nested letrec have not been unnested yet (in reduction) *)
 let rec clos (k:Fknormal.t) :t = match k with
-(*This is not needed for the first version of closure we consider : there is no higher-order function*)
-    (* | LetRec (f, a, b) -> (match a with
-        | LetRec  (y, a2, b2) -> clos (LetRec (y, clos a2, (clos (LetRec (x, b2, b)))))
-        | _ -> LetRec (x, clos a, clos b)) *)
+(* We now consider that there are no free variable inside our nested letrecs *)
+    | LetRec (fundef, t) ->
+        let (fname, fargs, fbody) = (fundef.name, fundef.args, fundef.body) in
+            (match fbody with
+            | LetRec (fundef2, t2) ->
+                let (newfundef : Fknormal.fundef) = {name = fname; args = fargs; body = t2} in
+                match (clos (LetRec (fundef2, Unit))) with
+                LetRec (f, _) ->
+                    let newfundef2 = {name = f.name; args = f.args; formal_fv = []; body = (clos_exp t2)} in
+                    LetRec (newfundef2, clos (LetRec (newfundef, t)))
+            (* | Let (x, a, b) -> *)
+            )
+    (* | Let (a, b, c) -> Let (a, clos b, clos c) *)
     (* | App (f, l) ->
         let rec clos_args l = match l with
             | [] -> []
             | t::q -> (clos t)::(clos_args q)
         in AppD (f, clos_args l) *)
-    | Unit -> Unit
-    | Bool a -> Bool a
-    | Int a ->  Int  a
-    | Float a -> Float a
-    | Not b -> Not (clos b)
-    | Neg b -> Neg (clos b)
-    | Sub (a, b) -> Sub (clos a, clos b)
-    | Add (a, b) -> Add (clos a, clos b)
-    | FAdd (a, b) -> FAdd (clos a, clos b)
-    | FNeg b -> FNeg (clos b)
-    | FSub (a, b) -> FSub (clos a, clos b)
-    | FMul (a, b) -> FMul (clos a, clos b)
-    | FDiv (a, b) -> FDiv (clos a, clos b)
-    | Eq (a, b) -> Eq (clos a, clos b)
-    | LE (a, b) -> LE (clos a, clos b)
-    | Var a -> Var a
-    (* |App (a,b) -> AppD (clos a, List.map clos b) *)
-    | IfEq (a, b, c) -> IfEq (clos a, clos b, clos c)
-    | IfLE (a, b, c) -> IfLE (clos a, clos b, clos c)
-    (* |IfBool (a, b, c) -> IfBool (clos a, clos b, clos c) *)
-    | Tuple a -> Tuple (List.map clos a)
-    (* |LetTuple (a, b, c) -> LetTuple (clos a, clos b, clos c) *)
-    | Array (a, b) -> Array (clos a, clos b)
-    | Get (a, b) -> Get (clos a, clos b)
-    | Put (a, b, c) -> Put (clos a, clos b, clos c)
-    (*/tmp*)
-    | Let (a, b, c) -> Let (a, clos b, clos c) (*OK*)
+
 
 (*
 let rec clos_toplevel k = match k with
