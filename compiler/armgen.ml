e791b4a01ee7cee018affc824efcda9ff4421762
Theo Barollet <theo.barollet@ensimag.fr>
2017-12-18 11:20:26 +0100

    preparing merging frontend and backend

---
+++ compiler/armgen.ml
@@ -0,0 +1,20 @@
+let print_arm l =
+  let s = (Parser.toplevel Lexer.token l) in
+  print_string (Syntax.to_arm_top s); print_newline ()
+
+let file f = 
+    let inchan = open_in f in
+    try
+        print_arm (Lexing.from_channel inchan);
+    close_in inchan
+    with e -> (close_in inchan; raise e)
+
+let () = 
+    let files = ref [] in
+    Arg.parse
+    [ ]
+    (fun s -> files := !files @ [s])
+    (Printf.sprintf "usage: %s filenames" Sys.argv.(0));
+  List.iter
+    (fun f -> ignore (file f))
+    !files
