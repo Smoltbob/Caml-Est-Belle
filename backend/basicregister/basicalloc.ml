bd305e99c4db6e46f56d7017552e7316484228d9
NingMosig <ning.tang@edu.univ-grenoble-alpes.fr>
2017-12-18 16:09:56 +0100

    change the basicalloc by hashtbl

--- backend/basicregister/basicalloc.ml
+++ backend/basicregister/basicalloc.ml
@@ -2,20 +2,32 @@ open Syntax;;
 
 let max_var_nb = 10 in
 let register_number = 10 in
-let variabletable = Array.make max_var_nb ("", 0) in
+let vartbl = Hashtbl.create max_var_nb in
 let address = ref 0 in
 let i = ref 0 in
 
+let int_of_bool b = if b then 1 else 0 in
+
+(* array tuple structure*)
+(*
 let allocVar x = 
-	while ((!i < (Array.length variabletable)) && ((fst variabletable.(!i)) <> "") && (x <> (fst variabletable.(!i)))) do
+	while ((!i < (Array.length vartbl)) && ((fst vartbl.(!i)) <> "") && (x <> (fst vartbl.(!i)))) do
 		i := !i + 1;
 	done;
-	if !i = (Array.length variabletable) then
-		(failwith "variabletable is full");
-	if ((fst variabletable.(!i)) = "") then
+	if !i = (Array.length vartbl) then
+		(failwith "vartbl is full");
+	if ((fst vartbl.(!i)) = "") then
 	    address := !address + 4;
-		variabletable.(!i) <- (x, !address)
+		vartbl.(!i) <- (x, !address)
 		(* TODO : for now, we just alloc type int*)		
+in
+*)
+
+(* hashtbl_tuple_structure: fst element in tuple is a boolean representing the var spilled or not, snd element is the adress or register *)
+let allocVar x =
+	if (not (Hashtbl.mem vartbl x)) then
+		address := !address + 4;
+		Hashtbl.add vartbl x (true, !address)
 in
 
 let rec regist prog table =
@@ -36,11 +48,11 @@ in
 
 let () =
 	let prog = Sub("a", Var("b")) in(* Parser.toplevel is the input*)
-	regist prog variabletable;
-	let j = ref 0 in
-	while (!j < (Array.length variabletable)) do
-	print_string (fst variabletable.(!j));
-	print_int (snd variabletable.(!j));
-	j := !j + 1;
-	done
+	regist prog vartbl;
+	print_string ("a");
+	print_int (int_of_bool (fst (Hashtbl.find vartbl "a")));
+	print_int (snd (Hashtbl.find vartbl "a"));
+	print_string ("b");
+	print_int (int_of_bool (fst (Hashtbl.find vartbl "b")));
+	print_int (snd (Hashtbl.find vartbl "b"));
 	in ()
