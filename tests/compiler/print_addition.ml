886b87bdbe9f2c89b88356dec3250379e2d5f659
Theo Barollet <theo.barollet@ensimag.fr>
2018-01-10 15:33:38 +0100

    created some tests in tests/compiler

--- tests/compiler/print_addition.ml
+++ tests/compiler/print_addition.ml
@@ -1,6 +1,10 @@
-let a = let x = 2+3 in
-print_int x in
-let b = let y = 1+2 in
-print_int y in
-let c = let z = x+y in
-print_int z in ()
+let x = 2 + 3 in
+let a = print_int x in
+let b = print_newline () in
+let y = 1 + 2 in
+let c = print_int y in
+let d = print_newline () in
+let z = x+y in
+let e = print_int z in
+let f = print_newline () in
+()
