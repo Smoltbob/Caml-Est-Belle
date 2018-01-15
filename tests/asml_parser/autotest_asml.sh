ab859ee4a8ca27b225787f51b03c223ce19f8a26
Jules Simon <jules.simon@ensimag.fr>
2018-01-15 16:03:01 +0100

    Moved tests

--- tests/asml_parser/autotest_asml.sh
+++ tests/asml_parser/autotest_asml.sh
@@ -32,30 +32,34 @@ while getopts "vp:t:d:" opt; do
     esac
 done
 
-#shift $((OPTIND-1))
-#[ "$1" == "--" ] && shift
-
+# File counter
+counter=0
+# Success counter
+okcounter=0
 # Going through asml files in the folder to execute them
 for folder in "$dir"/*/; do
     for file in "$folder"*.asml; do
+        counter=$((counter + 1))
         # Removing extention from filename
         filename=$(basename "$file")
         filename="${filename%.*}"
         # Printing filename + path
-        echo -e "File: \e[34m$file\e[0m"
+        echo -e "File: \\e[34m$file\\e[0m"
         # Printing output from parsing + ARM generation
         RESULT=$("$prog" "$file" 2>&1)
         # Printing the expected output
-        EXP=$(cat "$folder"expected_$typ/"$filename".exp)
+        EXP=$(cat "$folder"expected_"$typ"/"$filename".exp)
         if [[ $verb = 1 ]] || [[ "$RESULT" != "$EXP" ]]; then
-            echo -e "\e[33mOutput   : $RESULT\e[0m"
-            echo -e "\e[35mExpected : $EXP\e[0m"
+            echo -e "\\e[33mOutput   : $RESULT\\e[0m"
+            echo -e "\\e[35mExpected : $EXP\\e[0m"
         fi
         # Comparison between the two
         if [[ $RESULT == "$EXP" ]]; then
-            echo -e "\e[7mResult\e[27m \e[32mOK\e[0m"
+            echo -e "\\e[7mResult\\e[27m \\e[32mOK\\e[0m"
+            okcounter=$((okcounter + 1))
         else 
-            echo -e "\e[7mResult\e[27m \e[31mKO\e[0m"
+            echo -e "\\e[7mResult\\e[27m \\e[31mKO\\e[0m"
         fi
     done
 done
+echo -e "\\e[4m->" "$okcounter" tests passed successfully from "$counter" "($((100 * okcounter/counter))%)" "\\e[24m"
