d5b29743dcf412c1b0cde8cce77fac40ccffab87
Jules Simon <jules.simon@ensimag.fr>
2018-01-15 10:20:16 +0100

    Factorized barmspillgenerator

--- compiler/barmspillgenerator.ml
+++ compiler/barmspillgenerator.ml
@@ -110,42 +110,33 @@ let rec exp_to_arm exp dest =
                             sprintf "%s%s%s%s" store_arg1 store_arg2 load mov 
                 );
 
-    (* TODO factorise *)
+                
     | MemAff (id1, id2, id3) ->
             let saver7 = sprintf "\tstmfd sp!, {r7}\n" in
             let store_arg1 = sprintf "\tldr r4, [fp, #%i]\n" (fst (frame_position id1)) in
+            let prepstore = sprintf "\tldr r7, [fp, #%i]\n" (fst (frame_position id3)) in
+            let store = sprintf "\tstr r7, [r4, r5, lsl #2]\n" in
+            let restorer7 = sprintf "\tldmfd sp!, {r7}\n" in
             (match id2 with
             | Var id -> let store_arg2 = sprintf "\tldr r5, [fp, #%i]\n" (fst (frame_position id)) in
-                        let prepstore = sprintf "\tldr r7, [fp, #%i]\n" (fst (frame_position id3)) in
-                        let store = sprintf "\tstr r7, [r4, r5, lsl #2]\n" in
-                        let restorer7 = sprintf "\tldmfd sp!, {r7}\n" in
-                            sprintf "%s%s%s%s%s%s" saver7 store_arg1 store_arg2 prepstore store restorer7
+                        sprintf "%s%s%s%s%s%s" saver7 store_arg1 store_arg2 prepstore store restorer7
             | Int i ->  let store_arg2 = sprintf "\tmov r5, #%i\n" i in
-                        let prepstore = sprintf "\tldr r7, [fp, #%i]\n" (fst (frame_position id3)) in
-                        let store = sprintf "\tstr r7, [r4, r5, lsl #2]\n" in
-                        let restorer7 = sprintf "\tldmfd sp!, {r7}\n" in
                         sprintf "%s%s%s%s%s%s" saver7 store_arg1 store_arg2 prepstore store restorer7
             )
-    (* TODO factorise *)
+
     | If (id1, e1, asmt1, asmt2, comp) ->
             let counter = genif() in 
             let store_arg1 = sprintf "\tldr r4, [fp, #%i]\n" (fst (frame_position id1)) in
+            let cmpop = sprintf "\tcmp r4, r5\n" in
+            let branch1 = sprintf "\t%s if%s\n" comp counter in
+            let codeelse = sprintf "%s" (asmt_to_arm asmt2 dest) in
+            let branch2 = sprintf "\tb end%s\n\n" counter in
+            let codeif = sprintf "if%s:\n%s\n" counter (asmt_to_arm asmt1 dest) in
+            let endop = sprintf "end%s:\n" counter in
             (match e1 with
             | Var id -> let store_arg2 = sprintf "\tldr r5, [fp, #%i]\n" (fst (frame_position id)) in 
-                        let cmpop = sprintf "\tcmp r4, r5\n" in
-                        let branch1 = sprintf "\t%s if%s\n" comp counter in
-                        let codeelse = sprintf "%s" (asmt_to_arm asmt2 dest) in
-                        let branch2 = sprintf "\tb end%s\n\n" counter in
-                        let codeif = sprintf "if%s:\n%s\n" counter (asmt_to_arm asmt1 dest) in
-                        let endop = sprintf "end%s:\n" counter in
                         sprintf "%s%s%s%s%s%s%s%s" store_arg1 store_arg2 cmpop branch1 codeelse branch2 codeif endop
             | Int i  -> let store_arg2 = sprintf "\tmov r5,#%i\n" i in 
-                        let cmpop = sprintf "\tcmp r4, r5\n" in
-                        let branch1 = sprintf "\t%s if%s\n" comp counter in
-                        let codeelse = sprintf "%s" (asmt_to_arm asmt2 dest) in
-                        let branch2 = sprintf "\tb end%s\n\n" counter in
-                        let codeif = sprintf "if%s:\n%s\n" counter (asmt_to_arm asmt1 dest) in
-                        let endop = sprintf "end%s:\n" counter in
                         sprintf "%s%s%s%s%s%s%s%s" store_arg1 store_arg2 cmpop branch1 codeelse branch2 codeif endop
             )
     | Nop -> sprintf "\tnop\n"
