a43e4feb1edfde00b542a4f79d3959078135dddf
Sara <Sara.Lakah@etu.univ-grenoble-alpes.fr>
2018-01-15 13:50:03 +0100

    implement type checking: replace our old methods with new one. now we generate all equations then we solve it

--- compiler/ftype.ml
+++ compiler/ftype.ml
@@ -6,6 +6,12 @@ type t =
   | Fun of t list * t 
   | Tuple of t list
   | Array of t
-  | Var of t option ref
+  | Var of string
 
-let gentyp () = Var(ref None) 
+let ctr = ref 0
+
+let gentyp () = let newType= "t"^(Printf.sprintf "%d" !ctr)in incr ctr; 
+
+	Var(newType) 
+
+
